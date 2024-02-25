a7a6f45af25bc3d8eb493d43e630d404b964e192
Ariane Emory <ariane.emory@protonmail.com>
2024-02-25 09:29:24 -0500

    ...

--- src/geo.rs
+++ src/geo.rs
@@ -131,9 +131,9 @@ pub struct Line {
   pub start: Point,
   pub end: Point,
 }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
 type LineResult = std::result::Result<Line, ErrString>;
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
 impl Line {
   pub fn new(start: Point, end: Point) -> LineResult {
     if start == end {
@@ -151,53 +151,52 @@ impl Line {
     };
     Ok(Line { start, end })
   }
+
+  // pub fn is_horizontal(&self) -> bool {
+  //   self.start.is_horizontally_aligned_with(&self.end)
+  // }
+
+  // pub fn is_vertical(&self) -> bool {
+  //   self.start.is_vertically_aligned_with(&self.end)
+  // }
+
+  // pub fn is_parallel_to(&self, other: &Self) -> bool {
+  //   (self.is_horizontal() && other.is_horizontal()) || (self.is_vertical() && other.is_vertical())
+  // }
+
+  // pub fn could_pair_vertically_with(&self, other: Self) -> bool {
+  //   self.start.is_horizontally_aligned_with(&other.start) && self.length() == other.length()
+  // }
+
+  // pub fn could_pair_horizontally_with(&self, other: Self) -> bool {
+  //   self.start.is_vertically_aligned_with(&other.start) && self.length() == other.length()
+  // }
+
+  // pub fn length(&self) -> u64 {
+  //   if self.is_horizontal() {
+  //     self.end.col - self.start.col
+  //   } else {
+  //     self.end.line - self.start.line
+  //   }
+  // }
+
+  // // Whether lines are 'above' or to the left of each other is judged based on their start.
+  // pub fn is_above(&self, other: Self) -> bool {
+  //   self.start.is_above(&other.start)
+  // }
+
+  // pub fn is_below(&self, other: Self) -> bool {
+  //   self.start.is_below(&other.start)
+  // }
+
+  // pub fn is_left_of(&self, other: Self) -> bool {
+  //   self.start.is_left_of(&other.start)
+  // }
+
+  // pub fn is_right_of(&self, other: Self) -> bool {
+  //   self.start.is_right_of(&other.start)
+  // }
 }
-
-// pub fn is_horizontal(&self) -> bool {
-//   self.start.is_horizontally_aligned_with(&self.end)
-// }
-
-// pub fn is_vertical(&self) -> bool {
-//   self.start.is_vertically_aligned_with(&self.end)
-// }
-
-// pub fn is_parallel_to(&self, other: &Self) -> bool {
-//   (self.is_horizontal() && other.is_horizontal()) || (self.is_vertical() && other.is_vertical())
-// }
-
-// pub fn could_pair_vertically_with(&self, other: Self) -> bool {
-//   self.start.is_horizontally_aligned_with(&other.start) && self.length() == other.length()
-// }
-
-// pub fn could_pair_horizontally_with(&self, other: Self) -> bool {
-//   self.start.is_vertically_aligned_with(&other.start) && self.length() == other.length()
-// }
-
-// pub fn length(&self) -> u64 {
-//   if self.is_horizontal() {
-//     self.end.col - self.start.col
-//   } else {
-//     self.end.line - self.start.line
-//   }
-// }
-
-// // Whether lines are 'above' or to the left of each other is judged based on their start.
-// pub fn is_above(&self, other: Self) -> bool {
-//   self.start.is_above(&other.start)
-// }
-
-// pub fn is_below(&self, other: Self) -> bool {
-//   self.start.is_below(&other.start)
-// }
-
-// pub fn is_left_of(&self, other: Self) -> bool {
-//   self.start.is_left_of(&other.start)
-// }
-
-// pub fn is_right_of(&self, other: Self) -> bool {
-//   self.start.is_right_of(&other.start)
-// }
-// }
 
 impl Eq for Line {}
 
