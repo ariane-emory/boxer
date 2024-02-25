93f032031e80552982b78d679360a20a15dc0d2d
Ariane Emory <ariane.emory@protonmail.com>
2024-02-25 07:50:03 -0500

    ...

--- tests/geo_tests.rs
+++ tests/geo_tests.rs
@@ -6,14 +6,14 @@ mod tests {
 
   #[test]
   fn point_test() {
-    let above = Point::new(0, 0);
-    let center = Point::new(4, 4);
-    let below = Point::new(8, 8);
-    let left_of_center = Point::new(4, 0);
-    let right_of_center = Point::new(4, 8);
     let upper_left = Point::new(0, 0);
+    let upper_middle = Point::new(0, 0);
     let upper_right = Point::new(0, 8);
+    let left_of_center = Point::new(4, 0);
+    let center = Point::new(4, 4);
+    let right_of_center = Point::new(4, 8);
     let lower_left = Point::new(8, 0);
+    let lower_middle = Point::new(8, 8);
     let lower_right = Point::new(8, 8);
 
     assert_eq!(4, center.line);
