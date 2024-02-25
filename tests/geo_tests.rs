7ab6461760d1eb9fbef7410a1199fec751a277eb
Ariane Emory <ariane.emory@protonmail.com>
2024-02-25 08:59:46 -0500

    ...

--- tests/geo_tests.rs
+++ tests/geo_tests.rs
@@ -59,15 +59,15 @@ mod tests {
     assert!(center.is_bottom_aligned_with(&left_of_center));
     assert!(center.is_bottom_aligned_with(&right_of_center));
 
-    // assert!(!center.is_left_of(&center));
-    // assert!(!center.is_left_of(&left_of_center));
-    // assert!(!center.is_left_of(&lower_left));
-    // assert!(!center.is_left_of(&lower_middle));
-    // assert!(!center.is_left_of(&upper_left));
-    // assert!(!center.is_left_of(&upper_middle));
-    // assert!(center.is_left_of(&lower_right));
-    // assert!(center.is_left_of(&right_of_center));
-    // assert!(center.is_left_of(&upper_right));
+    assert!(!center.is_left_of(&center));
+    assert!(!center.is_left_of(&left_of_center));
+    assert!(!center.is_left_of(&lower_left));
+    assert!(!center.is_left_of(&lower_middle));
+    assert!(!center.is_left_of(&upper_left));
+    assert!(!center.is_left_of(&upper_middle));
+    assert!(center.is_left_of(&lower_right));
+    assert!(center.is_left_of(&right_of_center));
+    assert!(center.is_left_of(&upper_right));
 
     // assert!(!center.is_right_of(&center));
     // assert!(!center.is_right_of(&lower_middle));
