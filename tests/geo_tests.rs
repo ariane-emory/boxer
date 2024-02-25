06953d5fa0b10c8e4dd447848d89122ada83ad15
Ariane Emory <ariane.emory@protonmail.com>
2024-02-25 07:54:31 -0500

    ...

--- tests/geo_tests.rs
+++ tests/geo_tests.rs
@@ -7,14 +7,18 @@ mod tests {
   #[test]
   fn point_test() {
     let upper_left = Point::new(0, 0);
-    let upper_middle = Point::new(0, 0);
+    let upper_middle = Point::new(0, 4);
     let upper_right = Point::new(0, 8);
+
     let left_of_center = Point::new(4, 0);
     let center = Point::new(4, 4);
     let right_of_center = Point::new(4, 8);
+
     let lower_left = Point::new(8, 0);
     let lower_middle = Point::new(8, 8);
     let lower_right = Point::new(8, 8);
+
+    assert!(center.is_horizontally_aligned_with(upper_middle));
 
     assert_eq!(4, center.line);
   }
