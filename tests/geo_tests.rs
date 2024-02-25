6e11e7edc04398c8f59e1851f324f3eafb166dac
Ariane Emory <ariane.emory@protonmail.com>
2024-02-25 07:48:57 -0500

    ...

--- tests/geo_tests.rs
+++ tests/geo_tests.rs
@@ -2,12 +2,19 @@
 
 #[cfg(test)]
 mod tests {
-  use squares::geo::Point; // This brings `Point` into scope within the tests module
+  use squares::geo::Point;
 
   #[test]
   fn point_test() {
     let above = Point::new(0, 0);
     let center = Point::new(4, 4);
+    let below = Point::new(8, 8);
+    let left_of_center = Point::new(4, 0);
+    let right_of_center = Point::new(4, 8);
+    let upper_left = Point::new(0, 0);
+    let upper_right = Point::new(0, 8);
+    let lower_left = Point::new(8, 0);
+    let lower_right = Point::new(8, 8);
 
     assert_eq!(4, center.line);
   }
