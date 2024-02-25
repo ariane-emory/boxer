08c6e9cdf352c28badba5265ff079df6079353b3
Ariane Emory <ariane.emory@protonmail.com>
2024-02-25 11:46:18 -0500

    ...

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ fn main() {
   // 5 xxxxx       xxxxx
 
   let mut lines = vec![
-    Line::from_points(Point::new(0, 0), Point::new(4, 0)).unwrap(),
-    Line::from_points(Point::new(0, 5), Point::new(4, 5)).unwrap(),
-    Line::from_points(Point::new(0, 0), Point::new(0, 5)).unwrap(),
-    Line::from_points(Point::new(4, 0), Point::new(4, 5)).unwrap(),
-    Line::from_points(Point::new(5, 3), Point::new(9, 3)).unwrap(),
-    Line::from_points(Point::new(8, 0), Point::new(8, 1)).unwrap(),
+    Line::new(0, 0, 4, 0).unwrap(),
+    Line::new(0, 5, 4, 5).unwrap(),
+    Line::new(0, 0, 0, 5).unwrap(),
+    Line::new(4, 0, 4, 5).unwrap(),
+    Line::new(5, 3, 9, 3).unwrap(),
+    Line::new(8, 0, 8, 1).unwrap(),
   ];
 
   lines.sort(); // Sorts in place
