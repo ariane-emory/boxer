3cd8bd164e1449070438b6541ceb5dac72d18553
Ariane Emory <ariane.emory@protonmail.com>
2024-02-25 08:59:14 -0500

    ...

--- tests/geo_tests.rs
+++ tests/geo_tests.rs
@@ -39,15 +39,25 @@ mod tests {
     assert!(center.is_right_aligned_with(&lower_middle));
     assert!(center.is_right_aligned_with(&upper_middle));
 
-    // assert!(!center.is_vertically_aligned_with(&lower_left));
-    // assert!(!center.is_vertically_aligned_with(&lower_middle));
-    // assert!(!center.is_vertically_aligned_with(&lower_right));
-    // assert!(!center.is_vertically_aligned_with(&upper_left));
-    // assert!(!center.is_vertically_aligned_with(&upper_middle));
-    // assert!(!center.is_vertically_aligned_with(&upper_right));
-    // assert!(center.is_vertically_aligned_with(&center));
-    // assert!(center.is_vertically_aligned_with(&left_of_center));
-    // assert!(center.is_vertically_aligned_with(&right_of_center));
+    assert!(!center.is_top_aligned_with(&lower_left));
+    assert!(!center.is_top_aligned_with(&lower_middle));
+    assert!(!center.is_top_aligned_with(&lower_right));
+    assert!(!center.is_top_aligned_with(&upper_left));
+    assert!(!center.is_top_aligned_with(&upper_middle));
+    assert!(!center.is_top_aligned_with(&upper_right));
+    assert!(center.is_top_aligned_with(&center));
+    assert!(center.is_top_aligned_with(&left_of_center));
+    assert!(center.is_top_aligned_with(&right_of_center));
+
+    assert!(!center.is_bottom_aligned_with(&lower_left));
+    assert!(!center.is_bottom_aligned_with(&lower_middle));
+    assert!(!center.is_bottom_aligned_with(&lower_right));
+    assert!(!center.is_bottom_aligned_with(&upper_left));
+    assert!(!center.is_bottom_aligned_with(&upper_middle));
+    assert!(!center.is_bottom_aligned_with(&upper_right));
+    assert!(center.is_bottom_aligned_with(&center));
+    assert!(center.is_bottom_aligned_with(&left_of_center));
+    assert!(center.is_bottom_aligned_with(&right_of_center));
 
     // assert!(!center.is_left_of(&center));
     // assert!(!center.is_left_of(&left_of_center));
