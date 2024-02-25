d30c4e0fe21800b711aa7492f6417c736a75e696
Ariane Emory <ariane.emory@protonmail.com>
2024-02-25 07:58:06 -0500

    ...

--- tests/geo_tests.rs
+++ tests/geo_tests.rs
@@ -36,6 +36,15 @@ mod tests {
     assert!(!center.is_vertically_aligned_with(lower_middle));
     assert!(!center.is_vertically_aligned_with(lower_right));
 
+    assert!(center.is_left_of(right_of_center));
+    assert!(center.is_left_of(upper_right));
+    assert!(center.is_left_of(lower_right));
+    assert!(!center.is_left_of(left_of_center));
+    assert!(!center.is_left_of(upper_left));
+    assert!(!center.is_left_of(lower_left));
+    assert!(!center.is_left_of(upper_middle));
+    assert!(!center.is_left_of(lower_middle));
+
     assert_eq!(4, center.line);
   }
 }
