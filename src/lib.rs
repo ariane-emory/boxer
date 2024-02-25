9b359ab241fcefc5115103ea2933570f79c94488
Ariane Emory <ariane.emory@protonmail.com>
2024-02-25 07:02:00 -0500

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,1 @@
-// src/lib.rs
-#[path = "geo.rs"]
 pub mod geo;
--- tests/geo_tests.rs
+++ tests/geo_tests.rs
@@ -1,10 +1,8 @@
-use squares::geo::Point;
-
 #[cfg(test)]
 mod tests {
   #[test]
   fn point_test() {
-    let _point = Point::new(0, 0);
-    assert_eq!(4, 4);
+    let point = squares::geo::Point { line: 0, col: 0 };
+    assert_eq!(0, point.line);
   }
 }
