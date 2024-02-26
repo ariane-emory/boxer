1cfb83b54558cb09337a6562a6ae95c2f20a5d9b
Ariane Emory <ariane.emory@protonmail.com>
2024-02-26 15:55:41 -0500

    ...

---
+++ src/simple_geo/size.rs
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Size {
+  pub height: usize,
+  pub width: usize,
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl Size {
+  pub fn new(height: usize, width: usize) -> Size {
+    Size { height, width }
+  }
+
+  pub fn area(&self) -> usize {
+    if self.height == 1 && self.width == 1 {
+      1
+    } else if self.height == 1 {
+      self.width
+    } else if self.width == 1 {
+      self.height
+    } else {
+      self.height * self.width
+    }
+  }
+
+  pub fn is_tall(&self) -> bool {
+    self.height > self.width
+  }
+
+  pub fn is_wide(&self) -> bool {
+    self.height < self.width
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
