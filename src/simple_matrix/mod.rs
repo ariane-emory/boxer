ce143f9271a6130e7a10ed2fc17124f56cc6ed8f
Ariane Emory <ariane.emory@protonmail.com>
2024-02-27 09:25:34 -0500

    ...

--- src/simple_matrix/mod.rs
+++ src/simple_matrix/mod.rs
@@ -1,31 +1,11 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-pub fn make_matrix_uniform<T>(byte_matrix: &Vec<Vec<T>>, len: usize, val: T) -> Vec<Vec<T>>
-where
-  T: Copy,
-{
-  let mut new_matrix = Vec::new();
-
-  for row in byte_matrix {
-    let row_len = row.len();
-    if row_len < len {
-      let mut new_row = row.clone();
-      new_row.resize(len, val);
-      new_matrix.push(new_row);
-    } else {
-      let new_row = row[0..len.min(row_len)].to_vec();
-      new_matrix.push(new_row);
-    }
-  }
-
-  new_matrix
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 #[derive(Debug, PartialEq)]
 pub enum Rotation {
   Clockwise,
   CounterClockwise,
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 pub use Rotation::*;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -54,4 +34,26 @@ where
   }
 
   rotated_matrix
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub fn make_matrix_uniform<T>(byte_matrix: &Vec<Vec<T>>, len: usize, val: T) -> Vec<Vec<T>>
+where
+  T: Copy,
+{
+  let mut new_matrix = Vec::new();
+
+  for row in byte_matrix {
+    let row_len = row.len();
+    if row_len < len {
+      let mut new_row = row.clone();
+      new_row.resize(len, val);
+      new_matrix.push(new_row);
+    } else {
+      let new_row = row[0..len.min(row_len)].to_vec();
+      new_matrix.push(new_row);
+    }
+  }
+
+  new_matrix
 }
