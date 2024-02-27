a5bd80b074213773a8aa8ea8bd6f3d4c89ea1fe3
Ariane Emory <ariane.emory@protonmail.com>
2024-02-27 09:24:57 -0500

    ...

--- src/simple_matrix/mod.rs
+++ src/simple_matrix/mod.rs
@@ -19,3 +19,39 @@ where
 
   new_matrix
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#[derive(Debug, PartialEq)]
+pub enum Rotation {
+  Clockwise,
+  CounterClockwise,
+}
+pub use Rotation::*;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub fn rotate_matrix<T>(matrix: &[Vec<T>], rot: Rotation) -> Vec<Vec<T>>
+where
+  T: Copy,
+{
+  let num_rows = matrix.len();
+
+  if num_rows == 0 {
+    return vec![];
+  }
+
+  let num_cols = matrix[0].len();
+
+  let mut rotated_matrix = vec![vec![matrix[0][0]; num_rows]; num_cols];
+
+  for row in 0..num_rows {
+    for col in 0..num_cols {
+      if rot == Rotation::Clockwise {
+        rotated_matrix[col][num_rows - 1 - row] = matrix[row][col];
+      } else {
+        rotated_matrix[num_cols - 1 - col][row] = matrix[row][col];
+      }
+    }
+  }
+
+  rotated_matrix
+}
