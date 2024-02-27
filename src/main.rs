63bd44d7cd6ad33b446235bf0922f4a3894f5e96
Ariane Emory <ariane.emory@protonmail.com>
2024-02-27 09:22:26 -0500

    ...

--- src/main.rs
+++ src/main.rs
@@ -68,28 +68,6 @@ macro_rules! noisy_println {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-fn make_matrix_uniform<T>(byte_matrix: &Vec<Vec<T>>, len: usize, val: T) -> Vec<Vec<T>>
-where
-  T: Copy,
-{
-  let mut new_matrix = Vec::new();
-
-  for row in byte_matrix {
-    let row_len = row.len();
-    if row_len < len {
-      let mut new_row = row.clone();
-      new_row.resize(len, val);
-      new_matrix.push(new_row);
-    } else {
-      let new_row = row[0..len.min(row_len)].to_vec();
-      new_matrix.push(new_row);
-    }
-  }
-
-  new_matrix
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 fn process_matrix<T>(byte_matrix: &Vec<Vec<T>>, process: Box<dyn Fn(&Point, T)>)
 where
   T: Copy,
