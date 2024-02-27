33fd30ec6225f636786a016e1826c978b30d9754
Ariane Emory <ariane.emory@protonmail.com>
2024-02-27 12:50:08 -0500

    ...

--- src/main.rs
+++ src/main.rs
@@ -9,43 +9,8 @@ mod simple_geo;
 mod simple_matrix;
 use simple_geo::*;
 use simple_matrix::*;
-use std::cmp::max;
-use std::fs::File;
-use std::io::Seek;
-use std::io::SeekFrom;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self};
 type Point = simple_geo::Point;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-fn max_line_len(path: &str) -> io::Result<usize> {
-  let file = File::open(path)?;
-  let mut buf_reader = BufReader::new(file);
-  let mut pos = Point::new(0, 0);
-  let mut max_len = 0;
-
-  loop {
-    let buffer: &[u8] = buf_reader.fill_buf()?;
-
-    if buffer.is_empty() {
-      break;
-    }
-
-    for &byte in buffer {
-      if byte == b'\n' {
-        max_len = max(max_len, pos.col);
-        pos.col = 0;
-        pos.line += 1;
-      } else {
-        pos.col += 1;
-      }
-    }
-
-    let len = buffer.len();
-    buf_reader.consume(len);
-  }
-
-  Ok(max_len)
-}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 fn process_file(
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,7 @@
 mod errstring;
+mod max_line_len;
 mod noisy_println;
 
 pub use errstring::*;
+pub use max_line_len::*;
 pub use noisy_println::*;
