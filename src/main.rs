6379fce09099f5261643ab495b37e4cc9691db24
Ariane Emory <ariane.emory@protonmail.com>
2024-02-27 15:35:41 -0500

    ...

--- src/main.rs
+++ src/main.rs
@@ -63,32 +63,49 @@ impl LineMaker {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
 fn main() -> io::Result<()> {
-  let lm = Rc::new(RefCell::new(LineMaker::new(b'-')));
+  let horiz_lm = Rc::new(RefCell::new(LineMaker::new(b'-')));
+  // Clone for use inside the closure
+  let horiz_lm_clone = Rc::clone(&horiz_lm);
 
   let process_horiz = Box::new(move |pos: &Point, byte: &u8| {
     if 0 != (*byte & 128) {
       panic!("Found non-ASCII byte {} at {:?}", byte, pos);
     }
 
-    let mut lm = lm.borrow_mut();
+    // Use the cloned reference inside the closure
+    let mut lm = horiz_lm_clone.borrow_mut();
     lm.process(pos, byte);
 
     println!("Horiz {:?}: '{}'", pos, *byte as char);
   });
 
-  let lm = Rc::new(RefCell::new(LineMaker::new(b'|')));
+  let vert_lm = Rc::new(RefCell::new(LineMaker::new(b'|')));
+  // Clone for use inside the closure
+  let vert_lm_clone = Rc::clone(&vert_lm);
 
   let process_vert = Box::new(move |pos: &Point, byte: &u8| {
     let inverted_pos = Point::new(pos.line, pos.col);
 
-    let mut lm = lm.borrow_mut();
+    // Use the cloned reference inside the closure
+    let mut lm = vert_lm_clone.borrow_mut();
     lm.process(&inverted_pos, byte);
 
     println!("Vert  {:?}: '{}'", inverted_pos, *byte as char);
   });
 
+  // Assume process_file is correctly implemented
   let _ = process_file("./data/data.txt", process_horiz, process_vert);
+
+  // Use the original Rc references, which were not moved, to access the lines
+  for line in horiz_lm.borrow().lines.iter() {
+    println!("Horiz line: {:?}", line);
+  }
+
+  for line in vert_lm.borrow().lines.iter() {
+    println!("Vert line: {:?}", line);
+  }
 
   Ok(())
 }
