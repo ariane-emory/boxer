2521e2410210f43f77960f375ee408df0afd1362
Ariane Emory <ariane.emory@protonmail.com>
2024-02-27 17:06:33 -0500

    ...

--- src/main.rs
+++ src/main.rs
@@ -18,46 +18,47 @@ use std::rc::Rc;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 fn main() -> io::Result<()> {
-  let vert_lm = Rc::new(RefCell::new(LineMaker::new(b'|')));
-  let vert_lm_clone = Rc::clone(&vert_lm);
-
-  let process_vert = Box::new(move |pos: &Point, byte: &u8| {
-    let inverted_pos = Point::new(pos.line, pos.col);
-
-    let mut lm = vert_lm_clone.borrow_mut();
-    lm.process(&inverted_pos, byte);
-
-    println!("Vert  {:?}: '{}'", inverted_pos, *byte as char);
-  });
-
-  let horiz_lm = Rc::new(RefCell::new(LineMaker::new(b'-')));
-  let horiz_lm_clone = Rc::clone(&horiz_lm);
-
-  let process_horiz = Box::new(move |pos: &Point, byte: &u8| {
-    if 0 != (*byte & 128) {
-      panic!("Found non-ASCII byte {} at {:?}", byte, pos);
-    }
-
-    let mut lm = horiz_lm_clone.borrow_mut();
-    lm.process(pos, byte);
-
-    println!("Horiz {:?}: '{}'", pos, *byte as char);
-  });
-
-  let _ = process_file("./data/one.box", process_horiz, process_vert);
-
   let mut all_lines = Vec::new();
 
-  for line in horiz_lm.borrow().lines.iter() {
-    println!("Horiz line: {:?}", line);
-    all_lines.push(line.clone());
-  }
+  {
+    let vert_lm = Rc::new(RefCell::new(LineMaker::new(b'|')));
+    let vert_lm_clone = Rc::clone(&vert_lm);
 
-  for line in vert_lm.borrow().lines.iter() {
-    println!("Vert line:  {:?}", line);
-    all_lines.push(line.clone());
-  }
+    let process_vert = Box::new(move |pos: &Point, byte: &u8| {
+      let inverted_pos = Point::new(pos.line, pos.col);
 
+      let mut lm = vert_lm_clone.borrow_mut();
+      lm.process(&inverted_pos, byte);
+
+      println!("Vert  {:?}: '{}'", inverted_pos, *byte as char);
+    });
+
+    let horiz_lm = Rc::new(RefCell::new(LineMaker::new(b'-')));
+    let horiz_lm_clone = Rc::clone(&horiz_lm);
+
+    let process_horiz = Box::new(move |pos: &Point, byte: &u8| {
+      if 0 != (*byte & 128) {
+        panic!("Found non-ASCII byte {} at {:?}", byte, pos);
+      }
+
+      let mut lm = horiz_lm_clone.borrow_mut();
+      lm.process(pos, byte);
+
+      println!("Horiz {:?}: '{}'", pos, *byte as char);
+    });
+
+    let _ = process_file("./data/one.box", process_horiz, process_vert);
+
+    for line in horiz_lm.borrow().lines.iter() {
+      println!("Horiz line: {:?}", line);
+      all_lines.push(line.clone());
+    }
+
+    for line in vert_lm.borrow().lines.iter() {
+      println!("Vert line:  {:?}", line);
+      all_lines.push(line.clone());
+    }
+  }
   let mut rectangles = Vec::new();
   let mut leftover_lines = Vec::new();
 
