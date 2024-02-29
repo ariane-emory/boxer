3bdf2175a97c074509f34c43bd8255eba148a866
Ariane Emory <ariane.emory@protonmail.com>
2024-02-28 21:49:15 -0500

    ...

--- tests/simple_geo_tests.rs
+++ tests/simple_geo_tests.rs
@@ -495,27 +495,6 @@ mod tests {
   //////////////////////////////////////////////////////////////////////////////////////////////////
   #[test]
   fn find_rectangle_test() {
-    // //                   1111111111111111
-    // //   0123457890abcdef0123456789abcdef
-    // // 0 xxxxx  x    xxxxx
-    // // 1 x   x  x    x   x
-    // // 3 x   x       x   x
-    // // 4 x   xxxxxx  x   x
-    // // 5 xxxxx       xxxxx
-
-    // let mut lines = vec![
-    //   Line::new(0, 0, 0, 5).unwrap(),
-    //   Line::new(0, 0, 4, 0).unwrap(),
-    //   Line::new(0, 5, 4, 5).unwrap(),
-    //   Line::new(12, 0, 12, 5).unwrap(),
-    //   Line::new(12, 0, 16, 0).unwrap(),
-    //   Line::new(12, 5, 16, 5).unwrap(),
-    //   Line::new(16, 0, 16, 5).unwrap(),
-    //   Line::new(4, 0, 4, 5).unwrap(),
-    //   Line::new(5, 4, 10, 4).unwrap(),
-    //   Line::new(8, 0, 8, 1).unwrap(),
-    // ];
-
     //                   1111111111111111
     //   0123457890abcdef0123456789abcdef
     // 0 xxxxxxx
