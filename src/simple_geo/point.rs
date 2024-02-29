45aa4829f1ed800e7f32479182c0363c81c454c6
Ariane Emory <ariane.emory@protonmail.com>
2024-02-29 00:04:35 -0500

    ...

--- src/simple_geo/point.rs
+++ src/simple_geo/point.rs
@@ -45,4 +45,34 @@ impl Point {
     (self.col as isize - other.col as isize).abs() as usize
       + (self.line as isize - other.line as isize).abs() as usize
   }
+
+  pub fn invert(&self) -> Self {
+    Self::new(self.col, self.line)
+  }
+
+  pub fn offset(&self, line_offset: isize, col_offset: isize) -> Self {
+    // if adding the offsets to the existing line/column would over/underflow,
+    // something has gone wrong, panic.
+
+    if line_offset < 0 && self.line < (-line_offset) as usize {
+      panic!("Line underflow");
+    }
+
+    if col_offset < 0 && self.col < (-col_offset) as usize {
+      panic!("Column underflow");
+    }
+
+    if line_offset > 0 && self.line > (usize::MAX as isize - line_offset) as usize {
+      panic!("Line overflow");
+    }
+
+    if col_offset > 0 && self.col > (usize::MAX as isize - col_offset) as usize {
+      panic!("Column overflow");
+    }
+
+    Self::new(
+      (self.line as isize + line_offset) as usize,
+      (self.col as isize + col_offset) as usize,
+    )
+  }
 }
