e8589f9a4b6b7d2e1ba896294648df005813169f
Ariane Emory <ariane.emory@protonmail.com>
2024-02-29 04:55:00 -0500

    ...

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,13 @@ use std::io::{self};
 use std::rc::Rc;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-fn make_process_fun(
+fn make_process_fun<'a>(
   line_body_char: u8,
-  custom_printer: impl Fn(Point, u8) + 'static,
-) -> (Rc<RefCell<ConnectedLineMaker>>, Box<impl Fn(&Point, &u8)>) {
+  custom_printer: impl Fn(Point, u8) + 'a,
+) -> (
+  Rc<RefCell<ConnectedLineMaker>>,
+  Box<impl Fn(&Point, &u8) + 'a>,
+) {
   let lm = ConnectedLineMaker::new(line_body_char);
   let rc_lm = Rc::new(RefCell::new(lm));
   let rc_lm_twin = Rc::clone(&rc_lm);
