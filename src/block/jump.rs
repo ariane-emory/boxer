2916fe70b399d3fc690302b7253c0ea1bee117f8
Ariane Emory <ariane.emory@protonmail.com>
2024-03-01 08:34:29 -0500

    ...

--- src/block/jump.rs
+++ src/block/jump.rs
@@ -3,22 +3,23 @@ use crate::block::*;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 pub struct Jump<T: Copy + Default> {
   pub output: Signal<T>,
-  input: Option<Signal<T>>,
+  pub input: Option<Signal<T>>,
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 impl<T: Copy + Default> Jump<T> {
-  pub fn new(input: Option<Signal<T>>) -> Self {
-    if let Some(input) = input {
-      Jump {
-        output: new_signal(Default::default()),
-        input: Some(Rc::clone(&input)),
-      }
-    } else {
-      Jump {
-        output: new_signal(Default::default()),
-        input: None,
-      }
+  //  pub fn new(input: Option<Signal<T>>) -> Self {
+  pub fn new() -> Self {
+    // if let Some(input) = input {
+    //   Jump {
+    //     output: new_signal(Default::default()),
+    //     input: Some(Rc::clone(&input)),
+    //   }
+    // } else {
+    Jump {
+      output: new_signal(Default::default()),
+      input: None,
     }
+    //}
   }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -26,9 +27,10 @@ impl<T: Copy + Default> Block<T> for Jump<T> {
   fn step(&mut self) {
     if let Some(input) = &self.input {
       self.output.borrow_mut().set(*input.borrow().read());
-    } else {
-      self.output.borrow_mut().set(Default::default());
     }
+    // else {
+    //   self.output.borrow_mut().set(Default::default());
+    // }
   }
 
   fn output(&self) -> &Signal<T> {
