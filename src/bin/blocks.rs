a21eefd321e3f77d4e2d637b69ba9f1d938a6fa7
Ariane Emory <ariane.emory@protonmail.com>
2024-03-01 04:41:16 -0500

    ...

--- src/bin/blocks.rs
+++ src/bin/blocks.rs
@@ -2,21 +2,17 @@
 // #![allow(unused_variables)]
 // #![allow(unused_imports)]
 // #![allow(unused_mut)]
-//#![allow(dead_code)]
+// #![allow(dead_code)]
 
 use boxer::block;
 use boxer::block::*;
-use boxer::process_file::*;
-use boxer::simple_geo::find_rectangles;
-use boxer::simple_geo::line_methods::*;
-use std::cell::RefCell;
 use std::io::{self};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 fn main() -> io::Result<()> {
   // loop {
-  let mut left = block::Value::new(1);
-  let mut twenty = block::Value::new(20);
+  let left = block::Value::new(1);
+  let twenty = block::Value::new(20);
   let mut adder = block::MathAdd::new(&left.output, &left.output);
   let mut subber = block::MathSub::new(&twenty.output, &adder.output);
 
