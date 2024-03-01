ee26c6407aa77d549cae8d9e5e239b3d35e367eb
Ariane Emory <ariane.emory@protonmail.com>
2024-02-29 22:06:37 -0500

    ...

--- src/main.rs
+++ src/main.rs
@@ -23,81 +23,81 @@ static LINE_OFFSET: isize = 1;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 fn main() -> io::Result<()> {
-  loop {
-    let filename = "./data/tangle.box";
-    let mut rectangles = Vec::new();
-    let mut leftover_lines = Vec::new();
+  //loop {
+  let filename = "./data/tangle.box";
+  let mut rectangles = Vec::new();
+  let mut leftover_lines = Vec::new();
 
-    // all_lines scope:
+  // all_lines scope:
+  {
+    let mut all_lines = Vec::new();
+
+    // RefCell scope:
     {
-      let mut all_lines = Vec::new();
+      let (vert_linemaker, process_vert) = make_process_file_fun(b'|', |pos, byte| {
+        println!(
+          "Vert:    {:?}: '{}'",
+          pos.flip().offset_by(LINE_OFFSET, 0),
+          byte as char
+        );
+      });
+      let (horiz_linemaker, process_horiz) = make_process_file_fun(b'-', |pos, byte| {
+        println!(
+          "Horiz:   {:?}: '{}'",
+          pos.offset_by(LINE_OFFSET, 0),
+          byte as char
+        );
+      });
 
-      // RefCell scope:
-      {
-        let (vert_linemaker, process_vert) = make_process_file_fun(b'|', |pos, byte| {
-          println!(
-            "Vert:    {:?}: '{}'",
-            pos.flip().offset_by(LINE_OFFSET, 0),
-            byte as char
-          );
-        });
-        let (horiz_linemaker, process_horiz) = make_process_file_fun(b'-', |pos, byte| {
-          println!(
-            "Horiz:   {:?}: '{}'",
-            pos.offset_by(LINE_OFFSET, 0),
-            byte as char
-          );
-        });
+      process_file(filename, process_horiz, process_vert)?;
 
-        process_file(filename, process_horiz, process_vert)?;
+      println!("");
 
-        println!("");
+      // we'll offset the line by one so that the line numbers are consistent with emacs'
+      // line numbering.
+      for line in horiz_linemaker.borrow().lines.iter() {
+        let line = line.offset_by(LINE_OFFSET, 0);
+        println!("Horiz line: {:?}", line);
+        all_lines.push(line);
+      }
 
-        // we'll offset the line by one so that the line numbers are consistent with emacs'
-        // line numbering.
-        for line in horiz_linemaker.borrow().lines.iter() {
-          let line = line.offset_by(LINE_OFFSET, 0);
-          println!("Horiz line: {:?}", line);
-          all_lines.push(line);
-        }
+      // we'll offset the line by one so that the line numbers are consistent with emacs'
+      // line numbering. we'll also need to flip the row and column on the vertical lines,
+      // since the LineMaker will have made horizontal lines.
+      for line in vert_linemaker.borrow().lines.iter() {
+        let line = line.flip().offset_by(LINE_OFFSET, 0);
+        println!("Vert line:  {:?}", line);
+        all_lines.push(line);
+      }
+    } // End of RefCell scope.
 
-        // we'll offset the line by one so that the line numbers are consistent with emacs'
-        // line numbering. we'll also need to flip the row and column on the vertical lines,
-        // since the LineMaker will have made horizontal lines.
-        for line in vert_linemaker.borrow().lines.iter() {
-          let line = line.flip().offset_by(LINE_OFFSET, 0);
-          println!("Vert line:  {:?}", line);
-          all_lines.push(line);
-        }
-      } // End of RefCell scope.
+    find_rectangles(&all_lines, &mut rectangles, &mut leftover_lines);
+  } // End of all_lines scope.
 
-      find_rectangles(&all_lines, &mut rectangles, &mut leftover_lines);
-    } // End of all_lines scope.
-
-    for rect in rectangles.iter() {
-      println!("Found rectangle: {:?}", rect);
-    }
-
-    for line in leftover_lines.iter() {
-      println!("Leftover line: {:?}", line);
-    }
-
-    // let one = block::Value::new(1);
-    // let five = block::Value::new(5);
-    // let mut adder = block::MathAdd::new(&one.output, &five.output);
-    // println!("Adder: {}", adder.output.read());
-    // adder.step();
-    // println!("Adder: {}", adder.output.read());
-
-    // let mut flip = block::Value::new(false);
-    // let ten = block::Value::new(10);
-    // let mut ctr = block::RiseCounter::new(&flip.output, &ten.output);
-    // println!("Ctr: {}", ctr.count.read());
-    // ctr.step();
-    // println!("Ctr: {}", ctr.count.read());
-    // flip.output.set(true);
-    // ctr.step();
-    // println!("Ctr: {}", ctr.count.read());
+  for rect in rectangles.iter() {
+    println!("Found rectangle: {:?}", rect);
   }
+
+  for line in leftover_lines.iter() {
+    println!("Leftover line: {:?}", line);
+  }
+
+  // let one = block::Value::new(1);
+  // let five = block::Value::new(5);
+  // let mut adder = block::MathAdd::new(&one.output, &five.output);
+  // println!("Adder: {}", adder.output.read());
+  // adder.step();
+  // println!("Adder: {}", adder.output.read());
+
+  // let mut flip = block::Value::new(false);
+  // let ten = block::Value::new(10);
+  // let mut ctr = block::RiseCounter::new(&flip.output, &ten.output);
+  // println!("Ctr: {}", ctr.count.read());
+  // ctr.step();
+  // println!("Ctr: {}", ctr.count.read());
+  // flip.output.set(true);
+  // ctr.step();
+  // println!("Ctr: {}", ctr.count.read());
+  //  }
   Ok(())
 }
