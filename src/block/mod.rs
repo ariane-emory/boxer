3f1cd6c3186f4da2ac0018bf3fc35df7759249c6
Ariane Emory <ariane.emory@protonmail.com>
2024-03-01 03:39:09 -0500

    ...

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -460,33 +460,33 @@ impl Block for RisingTrigger {
 }
 
 
-// ////////////////////////////////////////////////////////////////////////////////////////////////////
-// pub struct FallingTrigger {
-//   input: Signal<bool>,
-//   pub output: Signal<bool>,
-// }
-// ////////////////////////////////////////////////////////////////////////////////////////////////////
-// impl FallingTrigger {
-//   pub fn new(input: Signal<bool>) -> Self {
-//     FallingTrigger {
-//       input,
-//       output: new_signal(false),
-//     }
-//   }
-// }
-// ////////////////////////////////////////////////////////////////////////////////////////////////////
-// impl Block for FallingTrigger {
-//   fn step(&mut self) {
-//     let last_state = *self.input.borrow().read();
-//     let input = *self.input.borrow().read();
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub struct FallingTrigger {
+  pub output: Signal<bool>,
+  input: Signal<bool>,
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl FallingTrigger {
+  pub fn new(input: &Signal<bool>) -> Self {
+    FallingTrigger {
+      output: new_signal(false),
+      input: Rc::clone(input),
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl Block for FallingTrigger {
+  fn step(&mut self) {
+    let last_state = *self.input.borrow().read();
+    let input = *self.input.borrow().read();
 
-//     if !input && last_state {
-//       self.output.borrow_mut().set(true);
-//     } else {
-//       self.output.borrow_mut().set(false);
-//     }
-//   }
-// }
+    if !input && last_state {
+      self.output.borrow_mut().set(true);
+    } else {
+      self.output.borrow_mut().set(false);
+    }
+  }
+}
 
 
 // ////////////////////////////////////////////////////////////////////////////////////////////////////
