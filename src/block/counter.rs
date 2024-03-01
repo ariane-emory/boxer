180d51e994e6cc2def2c72f7762a91f295dd9c0f
Ariane Emory <ariane.emory@protonmail.com>
2024-03-01 05:08:37 -0500

    ...

---
+++ src/block/counter.rs
@@ -0,0 +1,22 @@
+use crate::block::*;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub struct Counter {
+  pub output: Signal<usize>,
+  pub at_max: Signal<bool>,
+  input: Signal<bool>,
+  max: Signal<usize>,
+  last_state: bool,
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl Counter {
+  pub fn new(input: &Signal<bool>, max: &Signal<usize>) -> Self {
+    Counter {
+      output: new_signal(0),
+      at_max: new_signal(false),
+      input: Rc::clone(input),
+      max: Rc::clone(max),
+      last_state: false,
+    }
+  }
+}
