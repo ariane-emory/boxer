dcee918ff3dc981ae83be2480e82a91a2d412f95
Ariane Emory <ariane.emory@protonmail.com>
2024-03-01 05:41:27 -0500

    ...

--- src/block/select.rs
+++ src/block/select.rs
@@ -32,3 +32,66 @@ impl<T: Copy> Block<T> for Select<T> {
     &self.output
   }
 }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub struct Max<T: Copy + PartialOrd> {
+  pub output: Signal<T>,
+  left: Signal<T>,
+  right: Signal<T>,
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl<T: Copy + PartialOrd> Max<T> {
+  pub fn new(left: &Signal<T>, right: &Signal<T>) -> Self {
+    Max {
+      output: new_signal(*left.borrow().read()),
+      left: Rc::clone(left),
+      right: Rc::clone(right),
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl<T: Copy + PartialOrd> Block<T> for Max<T> {
+  fn step(&mut self) {
+    if *self.right.borrow().read() > *self.left.borrow().read() {
+      self.output.borrow_mut().set(*self.right.borrow().read());
+    } else {
+      self.output.borrow_mut().set(*self.left.borrow().read());
+    }
+  }
+
+  fn output(&self) -> &Signal<T> {
+    &self.output
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub struct Min<T: Copy + PartialOrd> {
+  pub output: Signal<T>,
+  left: Signal<T>,
+  right: Signal<T>,
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl<T: Copy + PartialOrd> Min<T> {
+  pub fn new(left: &Signal<T>, right: &Signal<T>) -> Self {
+    Min {
+      output: new_signal(*left.borrow().read()),
+      left: Rc::clone(left),
+      right: Rc::clone(right),
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl<T: Copy + PartialOrd> Block<T> for Min<T> {
+  fn step(&mut self) {
+    if *self.right.borrow().read() < *self.left.borrow().read() {
+      self.output.borrow_mut().set(*self.right.borrow().read());
+    } else {
+      self.output.borrow_mut().set(*self.left.borrow().read());
+    }
+  }
+
+  fn output(&self) -> &Signal<T> {
+    &self.output
+  }
+}
