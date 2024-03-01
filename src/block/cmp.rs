742a4ce20e881aa139dfa8c83eab917b19ead0fe
Ariane Emory <ariane.emory@protonmail.com>
2024-03-01 05:05:13 -0500

    ...

---
+++ src/block/cmp.rs
@@ -0,0 +1,62 @@
+use crate::block::*;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub struct GreaterThan<T: std::cmp::PartialOrd + Copy> {
+  pub output: Signal<bool>,
+  left: Signal<T>,
+  right: Signal<T>,
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl<T: std::cmp::PartialOrd + Copy> GreaterThan<T> {
+  pub fn new(left: &Signal<T>, right: &Signal<T>) -> Self {
+    GreaterThan {
+      output: new_signal(false),
+      left: Rc::clone(left),
+      right: Rc::clone(right),
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl<T: std::cmp::PartialOrd + Copy> Block<bool> for GreaterThan<T> {
+  fn step(&mut self) {
+    self
+      .output
+      .borrow_mut()
+      .set(*self.left.borrow().read() > *self.right.borrow().read());
+  }
+
+  fn output(&self) -> &Signal<bool> {
+    &self.output
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub struct LessThan<T: std::cmp::PartialOrd + Copy> {
+  pub output: Signal<bool>,
+  left: Signal<T>,
+  right: Signal<T>,
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl<T: std::cmp::PartialOrd + Copy> LessThan<T> {
+  pub fn new(left: &Signal<T>, right: &Signal<T>) -> Self {
+    LessThan {
+      output: new_signal(false),
+      left: Rc::clone(left),
+      right: Rc::clone(right),
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl<T: std::cmp::PartialOrd + Copy> Block<bool> for LessThan<T> {
+  fn step(&mut self) {
+    self
+      .output
+      .borrow_mut()
+      .set(*self.left.borrow().read() < *self.right.borrow().read());
+  }
+
+  fn output(&self) -> &Signal<bool> {
+    &self.output
+  }
+}
--- src/block/mod.rs
+++ src/block/mod.rs
@@ -1,10 +1,12 @@
 #![allow(dead_code)]
+pub mod cmp;
 pub mod latch;
 pub mod logic;
 pub mod math;
 pub mod timer;
 pub mod trigger;
 
+pub use cmp::*;
 pub use logic::*;
 pub use math::*;
 pub use timer::*;
@@ -103,68 +105,6 @@ impl<T: Copy> Block<T> for Select<T> {
   }
 
   fn output(&self) -> &Signal<T> {
-    &self.output
-  }
-}
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-pub struct GreaterThan<T: std::cmp::PartialOrd + Copy> {
-  pub output: Signal<bool>,
-  left: Signal<T>,
-  right: Signal<T>,
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////
-impl<T: std::cmp::PartialOrd + Copy> GreaterThan<T> {
-  pub fn new(left: &Signal<T>, right: &Signal<T>) -> Self {
-    GreaterThan {
-      output: new_signal(false),
-      left: Rc::clone(left),
-      right: Rc::clone(right),
-    }
-  }
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////
-impl<T: std::cmp::PartialOrd + Copy> Block<bool> for GreaterThan<T> {
-  fn step(&mut self) {
-    self
-      .output
-      .borrow_mut()
-      .set(*self.left.borrow().read() > *self.right.borrow().read());
-  }
-
-  fn output(&self) -> &Signal<bool> {
-    &self.output
-  }
-}
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-pub struct LessThan<T: std::cmp::PartialOrd + Copy> {
-  pub output: Signal<bool>,
-  left: Signal<T>,
-  right: Signal<T>,
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////
-impl<T: std::cmp::PartialOrd + Copy> LessThan<T> {
-  pub fn new(left: &Signal<T>, right: &Signal<T>) -> Self {
-    LessThan {
-      output: new_signal(false),
-      left: Rc::clone(left),
-      right: Rc::clone(right),
-    }
-  }
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////
-impl<T: std::cmp::PartialOrd + Copy> Block<bool> for LessThan<T> {
-  fn step(&mut self) {
-    self
-      .output
-      .borrow_mut()
-      .set(*self.left.borrow().read() < *self.right.borrow().read());
-  }
-
-  fn output(&self) -> &Signal<bool> {
     &self.output
   }
 }
