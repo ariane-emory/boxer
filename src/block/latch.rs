b4f8be0b0d0970531232e121e0e37014667e9b5a
Ariane Emory <ariane.emory@protonmail.com>
2024-03-01 12:36:36 -0500

    ...

--- src/block/latch.rs
+++ src/block/latch.rs
@@ -62,3 +62,77 @@ impl Block<bool> for RSLatch {
     &self.output
   }
 }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub struct JKFlipFlop {
+  output: Signal<bool>,
+  j_input: Signal<bool>,
+  k_input: Signal<bool>,
+  clock: Signal<bool>,
+  last_clock: bool,
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl JKFlipFlop {
+  pub fn new(j_input: &Signal<bool>, k_input: &Signal<bool>, clock: &Signal<bool>) -> Self {
+    JKFlipFlop {
+      output: new_signal(false),
+      j_input: Rc::clone(j_input),
+      k_input: Rc::clone(k_input),
+      clock: Rc::clone(clock),
+      last_clock: false,
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl Block<bool> for JKFlipFlop {
+  fn step(&mut self) {
+    if self.last_clock && !*self.clock.borrow().read() {
+      if *self.j_input.borrow().read() && *self.k_input.borrow().read() {
+        self.output.borrow_mut().set(!*self.output.borrow().read());
+      } else if *self.j_input.borrow().read() {
+        self.output.borrow_mut().set(true);
+      } else if *self.k_input.borrow().read() {
+        self.output.borrow_mut().set(false);
+      }
+    }
+    self.last_clock = *self.clock.borrow().read();
+  }
+
+  fn output(&self) -> &Signal<bool> {
+    &self.output
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub struct DFlipFlop {
+  output: Signal<bool>,
+  input: Signal<bool>,
+  clock: Signal<bool>,
+  last_clock: bool,
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl DFlipFlop {
+  pub fn new(input: &Signal<bool>, clock: &Signal<bool>) -> Self {
+    DFlipFlop {
+      output: new_signal(false),
+      input: Rc::clone(input),
+      clock: Rc::clone(clock),
+      last_clock: false,
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl Block<bool> for DFlipFlop {
+  fn step(&mut self) {
+    if self.last_clock && !*self.clock.borrow().read() {
+      self.output.borrow_mut().set(*self.input.borrow().read());
+    }
+    self.last_clock = *self.clock.borrow().read();
+  }
+
+  fn output(&self) -> &Signal<bool> {
+    &self.output
+  }
+}
