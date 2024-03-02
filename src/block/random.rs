ef77b30bde275d3fe36a5afd6fe73668f2441cfc
Ariane Emory <ariane.emory@protonmail.com>
2024-03-02 01:09:47 -0500

    ...

--- src/block/random.rs
+++ src/block/random.rs
@@ -16,11 +16,13 @@ impl RandomUsize {
   }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-impl HasSignal<usize> for RandomUsize {
+impl Steppable for RandomUsize {
   fn step(&mut self) {
     self.output.borrow_mut().set(rand::random());
   }
-
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl HasSignal<usize> for RandomUsize {
   fn output(&self) -> &Signal<usize> {
     &self.output
   }
