afe1ac8d566fa43cccdb4385b1d391bb30835e9f
Ariane Emory <ariane.emory@protonmail.com>
2024-03-03 13:47:44 -0500

    ...

--- src/simple_geo/connected_line.rs
+++ src/simple_geo/connected_line.rs
@@ -23,16 +23,12 @@ pub enum ConnectionType {
 impl fmt::Debug for ConnectedLine {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     let rotation_str = if self.is_vertical() { "V" } else { "H" };
-    let start_connects_to_str = format!("{:?}", self.start_connects_to);
-    let end_connects_to_str = format!("{:?}", self.end_connects_to);
+    let connection_str =
+      format!("{:?}⇼{:?}", self.start_connects_to, self.end_connects_to);
     write!(
       f,
-      "{}{:?} {:11}⇼{:11} {:?}",
-      rotation_str,
-      self.start,
-      start_connects_to_str,
-      end_connects_to_str,
-      self.end
+      "{}{:?} {:11} {:?}",
+      rotation_str, self.start, connection_str, self.end
     )
   }
 }
