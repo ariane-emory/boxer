cd82fbecb5da560dccdb8c33b9135d79549d7bea
Ariane Emory <ariane.emory@protonmail.com>
2024-03-04 05:55:08 -0500

    ...

--- src/simple_geo/rectangle.rs
+++ src/simple_geo/rectangle.rs
@@ -12,10 +12,7 @@ pub struct Rectangle {
 
 ////////////////////////////////////////////////////////////////////////////////
 impl fmt::Debug for Rectangle {
-  fn fmt(
-    &self,
-    f: &mut fmt::Formatter,
-  ) -> fmt::Result {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     write!(f, "Rectangle({:?}, {:?})", self.top_left, self.bottom_right)
   }
 }
@@ -33,10 +30,7 @@ impl Positional for Rectangle {
 
 ////////////////////////////////////////////////////////////////////////////////
 impl Rectangle {
-  pub fn new(
-    start: Point,
-    end: Point,
-  ) -> GeoResult<Self> {
+  pub fn new(start: Point, end: Point) -> GeoResult<Self> {
     // we want the 'start' point to be the top left corner and the 'end' point
     // to be the  bottom right corner... but, they might have been passed in
     // a different order, so we're going to create our own points using the
@@ -48,9 +42,11 @@ impl Rectangle {
 
     if bottom_right.left_bound() - top_left.left_bound() < 2 {
       Err(ErrString::new("Rectangle must be at least 3 columns wide"))
-    } else if bottom_right.upper_bound() - top_left.upper_bound() < 2 {
+    }
+    else if bottom_right.upper_bound() - top_left.upper_bound() < 2 {
       Err(ErrString::new("Rectangle must be at least 3 lines tall"))
-    } else {
+    }
+    else {
       Ok(Self {
         top_left,
         bottom_right,
@@ -74,5 +70,14 @@ impl Rectangle {
       top_left,
       bottom_right,
     })
+  }
+
+  pub fn contains<P: Positional>(&self, other: &P) -> bool {
+    let contained_rect = self.contained_rectangle().unwrap();
+
+    other.left_bound() >= contained_rect.left_bound()
+      && other.right_bound() <= contained_rect.right_bound()
+      && other.upper_bound() >= contained_rect.upper_bound()
+      && other.lower_bound() <= contained_rect.lower_bound()
   }
 }
