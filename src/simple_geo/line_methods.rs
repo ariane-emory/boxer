67bf36ee3149ad0c2feb02a93f1a9d9e015089ff
Ariane Emory <ariane.emory@protonmail.com>
2024-03-04 03:15:56 -0500

    offsetable

--- src/simple_geo/line_methods.rs
+++ src/simple_geo/line_methods.rs
@@ -2,13 +2,13 @@ use crate::simple_geo::*;
 
 ////////////////////////////////////////////////////////////////////////////////
 pub trait LineMethods: Positional {
-  //////////////////////////////////////////////////////////////////////////////
-  fn flip(&self) -> Self;
-  fn offset_by(
-    &self,
-    line_offset: isize,
-    col_offset: isize,
-  ) -> Self;
+  // ///////////////////////////////////////////////////////////////////////////
+  // /// fn flip(&self) -> Self;
+  // fn offset_by(
+  //   &self,
+  //   line_offset: isize,
+  //   col_offset: isize,
+  // ) -> Self;
 
   //////////////////////////////////////////////////////////////////////////////
   fn start(&self) -> Point {
@@ -28,7 +28,8 @@ pub trait LineMethods: Positional {
   fn orientation(&self) -> Orientation {
     if self.is_horizontal() {
       Horizontal
-    } else {
+    }
+    else {
       Vertical
     }
   }
@@ -43,50 +44,37 @@ pub trait LineMethods: Positional {
   }
 
   //////////////////////////////////////////////////////////////////////////////
-  fn is_parallel_to(
-    &self,
-    other: &Self,
-  ) -> bool {
+  fn is_parallel_to(&self, other: &Self) -> bool {
     (self.is_horizontal() && other.is_horizontal())
       || (self.is_vertical() && other.is_vertical())
   }
 
-  fn is_perpendicular_to(
-    &self,
-    other: &Self,
-  ) -> bool {
+  fn is_perpendicular_to(&self, other: &Self) -> bool {
     !self.is_parallel_to(other)
   }
 
   //////////////////////////////////////////////////////////////////////////////
-  fn is_coaligned_with(
-    &self,
-    other: &Self,
-  ) -> Option<Orientation> {
+  fn is_coaligned_with(&self, other: &Self) -> Option<Orientation> {
     if self.is_horizontally_coaligned_with(other) {
       Some(Horizontal)
-    } else if self.is_vertically_coaligned_with(other) {
+    }
+    else if self.is_vertically_coaligned_with(other) {
       Some(Vertical)
-    } else {
+    }
+    else {
       None
     }
   }
 
   //////////////////////////////////////////////////////////////////////////////
-  fn is_horizontally_coaligned_with(
-    &self,
-    other: &Self,
-  ) -> bool {
+  fn is_horizontally_coaligned_with(&self, other: &Self) -> bool {
     self.is_horizontal()
       && other.is_horizontal()
       && self.length() == other.length()
       && self.start().is_left_aligned_with(other)
   }
 
-  fn is_vertically_coaligned_with(
-    &self,
-    other: &Self,
-  ) -> bool {
+  fn is_vertically_coaligned_with(&self, other: &Self) -> bool {
     self.is_vertical()
       && other.is_vertical()
       && self.length() == other.length()
@@ -98,10 +86,7 @@ pub trait LineMethods: Positional {
     self.size().area()
   }
 
-  fn touches(
-    &self,
-    rect: &Rectangle,
-  ) -> bool {
+  fn touches(&self, rect: &Rectangle) -> bool {
     !(rect.point_is_corner(&self.start()) || rect.point_is_corner(&self.end()))
       && (self.start().overlaps(&rect.right_side())
         || self.start().overlaps(&rect.bottom_side())
@@ -109,10 +94,7 @@ pub trait LineMethods: Positional {
         || self.end().overlaps(&rect.top_side()))
   }
 
-  fn is_connected_to<T: LineMethods>(
-    &self,
-    other_line: &T,
-  ) -> bool {
+  fn is_connected_to<T: LineMethods>(&self, other_line: &T) -> bool {
     self.start() == other_line.start()
       || self.start() == other_line.end()
       || self.end() == other_line.start()
