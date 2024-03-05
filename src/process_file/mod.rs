404810cd6f0749a041b249508140c18cb047fcfa
Ariane Emory <ariane.emory@protonmail.com>
2024-03-05 13:21:12 -0500

    ...

--- src/process_file/mod.rs
+++ src/process_file/mod.rs
@@ -108,96 +108,98 @@ pub fn process_file(path: &str) -> Result<()> {
   println!("================================================================================");
   println!("");
 
-  let mut free_lines = free_lines;
-  free_lines.sort();
-  free_lines.reverse();
+  let free_lines = merge_interrupted_lines(free_lines);
 
-  let mut merged_horizontal_lines: Vec<ConnectedLine> = Vec::new();
-  let mut horizontal_lines = free_lines
-    .iter()
-    .filter(|line| line.is_horizontal())
-    .cloned()
-    .collect::<Vec<ConnectedLine>>();
-  horizontal_lines
-    .iter()
-    .for_each(|line| println!("Horizontal Line: {:?}", line));
+  // let mut free_lines = free_lines;
+  // free_lines.sort();
+  // free_lines.reverse();
 
-  let mut merged_vertical_lines: Vec<ConnectedLine> = Vec::new();
-  let mut vertical_lines = free_lines
-    .iter()
-    .filter(|line| line.is_vertical())
-    .cloned()
-    .collect::<Vec<ConnectedLine>>();
-  vertical_lines
-    .iter()
-    .for_each(|line| println!("Vertical Line:   {:?}", line));
+  // let mut merged_horizontal_lines: Vec<ConnectedLine> = Vec::new();
+  // let mut horizontal_lines = free_lines
+  //   .iter()
+  //   .filter(|line| line.is_horizontal())
+  //   .cloned()
+  //   .collect::<Vec<ConnectedLine>>();
+  // horizontal_lines
+  //   .iter()
+  //   .for_each(|line| println!("Horizontal Line: {:?}", line));
 
-  while let Some(mut line) = horizontal_lines.pop() {
-    println!("\nLooking for merges for {:?}...", line);
+  // let mut merged_vertical_lines: Vec<ConnectedLine> = Vec::new();
+  // let mut vertical_lines = free_lines
+  //   .iter()
+  //   .filter(|line| line.is_vertical())
+  //   .cloned()
+  //   .collect::<Vec<ConnectedLine>>();
+  // vertical_lines
+  //   .iter()
+  //   .for_each(|line| println!("Vertical Line:   {:?}", line));
 
-    while let Some(other) = horizontal_lines.remove_if(|o| {
-      line.end_connects_to == Wall
-        && line.end == o.start
-        && o.start_connects_to == Wall
-    }) {
-      println!("Could merge with {:?}.", other);
+  // while let Some(mut line) = horizontal_lines.pop() {
+  //   println!("\nLooking for merges for {:?}...", line);
 
-      line = ConnectedLine::new(
-        line.orientation,
-        line.start,
-        other.end,
-        line.start_connects_to,
-        other.end_connects_to,
-      )
-      .unwrap();
+  //   while let Some(other) = horizontal_lines.remove_if(|o| {
+  //     line.end_connects_to == Wall
+  //       && line.end == o.start
+  //       && o.start_connects_to == Wall
+  //   }) {
+  //     println!("Could merge with {:?}.", other);
 
-      println!("Merged into: {:?}", line);
-    }
+  //     line = ConnectedLine::new(
+  //       line.orientation,
+  //       line.start,
+  //       other.end,
+  //       line.start_connects_to,
+  //       other.end_connects_to,
+  //     )
+  //     .unwrap();
 
-    merged_horizontal_lines.push(line);
-  }
+  //     println!("Merged into: {:?}", line);
+  //   }
 
-  println!("");
+  //   merged_horizontal_lines.push(line);
+  // }
 
-  merged_horizontal_lines
-    .iter()
-    .for_each(|line| println!("Merged Horizontal Line: {:?}", line));
+  // println!("");
 
-  while let Some(mut line) = vertical_lines.pop() {
-    println!("\nLooking for merges for {:?}...", line);
+  // merged_horizontal_lines
+  //   .iter()
+  //   .for_each(|line| println!("Merged Horizontal Line: {:?}", line));
 
-    while let Some(other) = vertical_lines.remove_if(|o| {
-      line.end_connects_to == Wall
-        && line.end == o.start
-        && o.start_connects_to == Wall
-    }) {
-      println!("Could merge with {:?}.", other);
+  // while let Some(mut line) = vertical_lines.pop() {
+  //   println!("\nLooking for merges for {:?}...", line);
 
-      line = ConnectedLine::new(
-        line.orientation,
-        line.start,
-        other.end,
-        line.start_connects_to,
-        other.end_connects_to,
-      )
-      .unwrap();
+  //   while let Some(other) = vertical_lines.remove_if(|o| {
+  //     line.end_connects_to == Wall
+  //       && line.end == o.start
+  //       && o.start_connects_to == Wall
+  //   }) {
+  //     println!("Could merge with {:?}.", other);
 
-      println!("Merged into: {:?}", line);
-    }
+  //     line = ConnectedLine::new(
+  //       line.orientation,
+  //       line.start,
+  //       other.end,
+  //       line.start_connects_to,
+  //       other.end_connects_to,
+  //     )
+  //     .unwrap();
 
-    merged_vertical_lines.push(line);
-  }
+  //     println!("Merged into: {:?}", line);
+  //   }
 
-  println!("");
+  //   merged_vertical_lines.push(line);
+  // }
 
-  merged_vertical_lines
-    .iter()
-    .for_each(|line| println!("Merged Vertical Line: {:?}", line));
+  // println!("");
 
-  let mut all_merged_lines = Vec::new();
-  all_merged_lines.extend(merged_horizontal_lines);
-  all_merged_lines.extend(merged_vertical_lines);
-  all_merged_lines.sort();
+  // merged_vertical_lines
+  //   .iter()
+  //   .for_each(|line| println!("Merged Vertical Line: {:?}", line));
+
+  // let mut all_merged_lines = Vec::new();
+  // all_merged_lines.extend(merged_horizontal_lines);
+  // all_merged_lines.extend(merged_vertical_lines);
+  // all_merged_lines.sort();
 
   // if false {
   //   println!("");
