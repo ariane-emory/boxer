b00b1597f04da4186ac3ca64b1d94842822b4419
Ariane Emory <ariane.emory@protonmail.com>
2024-03-05 06:29:06 -0500

    ...

--- src/process_file/mod.rs
+++ src/process_file/mod.rs
@@ -1,30 +1,18 @@
-use crate::noisy_print;
-use crate::util::noisy_print;
-//use crate::noisy_println;
-//use crate::util::noisy_println;
-
+mod extract_lines_and_words;
 mod make_process_bidirectionally_fun;
-use make_process_bidirectionally_fun::make_process_bidirectionally_fun;
 
-#[allow(unused_imports)]
-use crate::simple_geo::find_rectangles;
+//use crate::simple_geo::find_rectangles;
 use crate::simple_geo::ConnectedLine;
-use crate::simple_geo::Flippable;
 use crate::simple_geo::LineMethods;
 use crate::simple_geo::Offsetable;
-//use crate::simple_geo::Orientation;
-use crate::simple_geo::Orientation::*;
-use crate::simple_geo::Point;
 use crate::simple_geo::Word;
-use crate::util::ErrString;
-//use crate::simple_geo::Rectangle;
 use crate::simple_matrix::*;
 use crate::util::vec_utils::Removeql;
 use crate::util::vec_utils::SortedInsert;
-use std::io::Result;
 
-/////////////////////////////////////////////////////////////////////////////////
-static LINE_OFFSET: isize = 1;
+pub use extract_lines_and_words::extract_lines_and_words;
+pub use make_process_bidirectionally_fun::make_process_bidirectionally_fun;
+use std::io::Result;
 
 /////////////////////////////////////////////////////////////////////////////////
 pub fn add_null_sentinels_to_normalized_matrix(
@@ -188,62 +176,4 @@ fn merge_length_1_lines(
   }
 
   (lines, words)
-}
-
-/////////////////////////////////////////////////////////////////////////////////
-fn extract_lines_and_words(
-  normalized_matrix: &Vec<Vec<u8>>,
-) -> (Vec<ConnectedLine>, Vec<Word>) {
-  let mut free_lines = Vec::new();
-  let mut words = Vec::new();
-  let flip_line = |cl: ConnectedLine| cl.flip();
-  let do_nothing_to_line = |line: ConnectedLine| line;
-  let do_nothing_to_word = |wrd: Word| wrd;
-  let log_orientation = |ori| {
-    move || {
-      noisy_print!("\n[{:12?}] ", ori);
-    }
-  };
-  let is_non_ascii_byte = |byte| {
-    (byte & 128 != 0)
-      .then(|| ErrString::new(&format!("Non-ASCII byte {}", byte)))
-  };
-  let offset_line = |pos: Point| pos.offset_by(LINE_OFFSET, 0);
-  let offset_column = |pos: Point| pos.offset_by(0, LINE_OFFSET);
-
-  let (vert_linemaker, process_vert_fun) = make_process_bidirectionally_fun(
-    b'|',
-    b'-',
-    false,
-    false,
-    is_non_ascii_byte,
-    offset_column,
-    flip_line,
-    do_nothing_to_word,
-    log_orientation(Vertical),
-  );
-
-  let (horiz_linemaker, process_horiz_fun) = make_process_bidirectionally_fun(
-    b'-',
-    b'|',
-    true,
-    true,
-    is_non_ascii_byte,
-    offset_line,
-    do_nothing_to_line,
-    do_nothing_to_word,
-    log_orientation(Horizontal),
-  );
-
-  process_matrix_bidirectionally(
-    normalized_matrix,
-    process_horiz_fun,
-    process_vert_fun,
-  );
-
-  words.extend(horiz_linemaker.borrow().words.iter().cloned());
-  free_lines.extend(horiz_linemaker.borrow().lines.iter());
-  free_lines.extend(vert_linemaker.borrow().lines.iter());
-
-  (free_lines, words)
 }
