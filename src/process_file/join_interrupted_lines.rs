58acdf80046dfd5e192997b87903cfa28851a066
Ariane Emory <ariane.emory@protonmail.com>
2024-03-05 14:05:47 -0500

    ...

--- src/process_file/join_interrupted_lines.rs
+++ src/process_file/join_interrupted_lines.rs
@@ -4,9 +4,7 @@ use crate::simple_geo::Orientation::*;
 use crate::util::RemoveIf;
 
 ////////////////////////////////////////////////////////////////////////////////
-pub fn join_all_interrupted_lines(
-  lines: Vec<ConnectedLine>,
-) -> Vec<ConnectedLine> {
+pub fn join_interrupted_lines(lines: Vec<ConnectedLine>) -> Vec<ConnectedLine> {
   let horizontal_lines = lines
     .iter()
     .filter(|cl| cl.orientation == Horizontal)
@@ -19,8 +17,10 @@ pub fn join_all_interrupted_lines(
     .cloned()
     .collect::<Vec<ConnectedLine>>();
 
-  let horizontal_lines = join_interrupted_lines(horizontal_lines);
-  let vertical_lines = join_interrupted_lines(vertical_lines);
+  let horizontal_lines =
+    join_similarly_oriented_interrupted_lines(horizontal_lines);
+  let vertical_lines =
+    join_similarly_oriented_interrupted_lines(vertical_lines);
   let mut lines = Vec::new();
   lines.extend(horizontal_lines);
   lines.extend(vertical_lines);
@@ -28,7 +28,8 @@ pub fn join_all_interrupted_lines(
   lines
 }
 
-pub fn join_interrupted_lines(
+// This function is meant to be passed a Vec of lines whose orientations match:
+fn join_similarly_oriented_interrupted_lines(
   mut lines: Vec<ConnectedLine>,
 ) -> Vec<ConnectedLine> {
   let mut merged_lines: Vec<ConnectedLine> = Vec::new();
@@ -37,12 +38,10 @@ pub fn join_interrupted_lines(
   while let Some(mut line) = lines.pop() {
     println!("Looking for merges for {:?}...", line);
 
-    if line.end_connects_to == Wall {
-      while let Some(other) = lines.remove_if(|o| {
-        line.orientation == o.orientation
-          && line.end == o.start
-          && o.start_connects_to == Wall
-      }) {
+    while line.end_connects_to == Wall {
+      if let Some(other) =
+        lines.remove_if(|o| line.end == o.start && o.start_connects_to == Wall)
+      {
         println!("Could merge with {:?}.", other);
 
         line = ConnectedLine::new(
