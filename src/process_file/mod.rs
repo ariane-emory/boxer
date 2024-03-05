cfd0353a9bf36fe8989bd61a463d626f1d6f2741
Ariane Emory <ariane.emory@protonmail.com>
2024-03-05 06:14:49 -0500

    ...

--- src/process_file/mod.rs
+++ src/process_file/mod.rs
@@ -3,6 +3,9 @@ use crate::util::noisy_print;
 //use crate::noisy_println;
 //use crate::util::noisy_println;
 
+mod make_process_bidirectionally_fun;
+use make_process_bidirectionally_fun::make_process_bidirectionally_fun;
+
 use crate::connected_line_maker::ConnectedLineMaker;
 #[allow(unused_imports)]
 use crate::simple_geo::find_rectangles;
@@ -25,41 +28,6 @@ use std::rc::Rc;
 
 /////////////////////////////////////////////////////////////////////////////////
 static LINE_OFFSET: isize = 1;
-
-/////////////////////////////////////////////////////////////////////////////////
-pub fn make_process_bidirectionally_fun<'a>(
-  line_body_char: u8,
-  wall_char: u8,
-  collect_words: bool,
-  allow_length_one: bool,
-  is_invalid_byte: impl Fn(u8) -> Option<ErrString> + 'a,
-  pos_preprocessor: impl Fn(Point) -> Point + 'a,
-  line_postprocessor: impl Fn(ConnectedLine) -> ConnectedLine + 'a,
-  word_postprocessor: impl Fn(Word) -> Word + 'a,
-  custom_printer: impl Fn() + 'a,
-) -> (Rc<RefCell<ConnectedLineMaker<'a>>>, impl Fn(&Point, &u8) + 'a) {
-  let linemaker = ConnectedLineMaker::new(
-    line_body_char,
-    wall_char,
-    collect_words,
-    allow_length_one,
-    line_postprocessor,
-    word_postprocessor,
-  );
-  let rc_linemaker = Rc::new(RefCell::new(linemaker));
-  let rc_linemaker_twin = Rc::clone(&rc_linemaker);
-
-  (rc_linemaker, move |pos: &Point, byte: &u8| {
-    let pos = pos_preprocessor(*pos);
-
-    if let Some(err) = is_invalid_byte(*byte) {
-      panic!("{} at {:?}", err, pos);
-    }
-
-    custom_printer();
-    rc_linemaker_twin.borrow_mut().process(pos, *byte);
-  })
-}
 
 /////////////////////////////////////////////////////////////////////////////////
 pub fn add_null_sentinels_to_normalized_matrix(
