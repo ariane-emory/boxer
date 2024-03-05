c70a1346ab208f0fd8f55a844c2cf13c53684226
Ariane Emory <ariane.emory@protonmail.com>
2024-03-05 07:59:21 -0500

    ...

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,6 +9,6 @@ use std::io::{self};
 
 ////////////////////////////////////////////////////////////////////////////////
 fn main() -> io::Result<()> {
-  process_file("./data/temp.box")?;
+  process_file("./data/complex.box")?;
   Ok(())
 }
