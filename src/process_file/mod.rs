9c6fffaaa42727f8fc5b39c5d64685dfe112cd1d
Ariane Emory <ariane.emory@protonmail.com>
2024-03-05 08:23:03 -0500

    ...

--- src/process_file/mod.rs
+++ src/process_file/mod.rs
@@ -3,10 +3,10 @@ mod extract_lines_and_words;
 mod make_process_bidirectionally_fun;
 mod merge_length_1_lines_with_words;
 
-//use crate::simple_geo::find_rectangles;
-//use crate::simple_geo::analyze_chain;
 use crate::simple_geo::chain_get_network;
 use crate::simple_geo::find_chains;
+use crate::simple_geo::find_rectangles;
+use crate::simple_geo::ConnectedLine;
 use crate::simple_matrix::matrix_max_row_len;
 use crate::simple_matrix::normalize_matrix_width;
 use crate::simple_matrix::read_file_to_byte_matrix;
@@ -37,47 +37,47 @@ pub fn process_file(path: &str) -> Result<()> {
     .iter()
     .for_each(|word| println!("Word:               {:?}", word));
 
-  // println!("");
-  // println!("================================================================================");
-  // println!("Finding rectangles in:");
-  // println!("================================================================================");
+  println!("");
+  println!("================================================================================");
+  println!("Finding rectangles in:");
+  println!("================================================================================");
 
-  // let mut non_rectangle_candidate_lines: Vec<ConnectedLine> = Vec::new();
-  // non_rectangle_candidate_lines
-  //   .extend(free_lines.iter().filter(|cl| !cl.is_corner_connected()));
+  let mut non_rectangle_candidate_lines: Vec<ConnectedLine> = Vec::new();
+  non_rectangle_candidate_lines
+    .extend(free_lines.iter().filter(|cl| !cl.is_corner_connected()));
 
-  // let mut rectangle_candidate_lines: Vec<ConnectedLine> = free_lines;
-  // rectangle_candidate_lines.retain(ConnectedLine::is_corner_connected);
+  let mut rectangle_candidate_lines: Vec<ConnectedLine> = free_lines;
+  rectangle_candidate_lines.retain(ConnectedLine::is_corner_connected);
 
-  // non_rectangle_candidate_lines
-  //   .iter()
-  //   .for_each(|line| println!("Non-Candidate Line: {:?}", line));
-  // rectangle_candidate_lines
-  //   .iter()
-  //   .for_each(|line| println!("Candidate Line:     {:?}", line));
-  // words
-  //   .iter()
-  //   .for_each(|word| println!("Word:               {:?}", word));
+  non_rectangle_candidate_lines
+    .iter()
+    .for_each(|line| println!("Non-Candidate Line: {:?}", line));
+  rectangle_candidate_lines
+    .iter()
+    .for_each(|line| println!("Candidate Line:     {:?}", line));
+  words
+    .iter()
+    .for_each(|word| println!("Word:               {:?}", word));
 
-  // println!("");
-  // println!("================================================================================");
-  // println!("Found:");
-  // println!("================================================================================");
+  println!("");
+  println!("================================================================================");
+  println!("Found:");
+  println!("================================================================================");
 
-  // let (rectangles, mut free_lines) =
-  //   find_rectangles(rectangle_candidate_lines, false);
+  let (rectangles, mut free_lines) =
+    find_rectangles(rectangle_candidate_lines, false);
 
-  // free_lines.extend(non_rectangle_candidate_lines);
+  free_lines.extend(non_rectangle_candidate_lines);
 
-  // free_lines
-  //   .iter()
-  //   .for_each(|line| println!("Free Line:          {:?}", line));
-  // rectangles
-  //   .iter()
-  //   .for_each(|rect| println!("Rectangle:          {:?}", rect));
-  // words
-  //   .iter()
-  //   .for_each(|word| println!("Word:               {:?}", word));
+  free_lines
+    .iter()
+    .for_each(|line| println!("Free Line:          {:?}", line));
+  rectangles
+    .iter()
+    .for_each(|rect| println!("Rectangle:          {:?}", rect));
+  words
+    .iter()
+    .for_each(|word| println!("Word:               {:?}", word));
 
   println!("");
   println!("================================================================================");
