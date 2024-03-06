e096215c33475b7238360f6ac36c21a4e6c456f2
Ariane Emory <ariane.emory@protonmail.com>
2024-03-05 19:55:09 -0500

    correct sort but in unwated l-to-r/top-to-bottom order

--- src/process_file/join_interrupted_lines.rs
+++ src/process_file/join_interrupted_lines.rs
@@ -44,46 +44,6 @@ pub fn join_interrupted_lines(lines: Vec<ConnectedLine>) -> Vec<ConnectedLine> {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// This function is meant to be passed a Vec of lines whose orientations match:
-#[allow(dead_code)]
-pub fn good_join_similarly_oriented_interrupted_lines(
-  mut lines: Vec<ConnectedLine>,
-) -> Vec<ConnectedLine> {
-  let mut merged_lines: Vec<ConnectedLine> = Vec::new();
-  lines.sort();
-  lines.reverse();
-  while let Some(mut line) = lines.pop() {
-    println!("\nLooking for merges for {:?}...", line);
-
-    while let Some(other) = lines.remove_if(|o| {
-      line.end_connects_to == Wall
-        && line.orientation == o.orientation
-        && line.start == o.end
-        && o.start_connects_to == Wall
-    }) {
-      println!("  Will merge with {:?}.", other);
-
-      line = ConnectedLine::new(
-        line.orientation,
-        line.start,
-        other.end,
-        line.start_connects_to,
-        other.end_connects_to,
-      )
-      .unwrap();
-
-      println!("  Merged into: {:?}", line);
-    }
-
-    merged_lines.push(line);
-  }
-  merged_lines
-}
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////
 #[allow(dead_code)]
 fn join_similarly_oriented_interrupted_lines(
   mut lines: Vec<ConnectedLine>,
@@ -93,10 +53,10 @@ fn join_similarly_oriented_interrupted_lines(
   while let Some(mut line) = lines.pop() {
     println!("\nLooking for merges for       {:?}...", line);
 
-    while line.start_connects_to == Wall {
+    while line.end_connects_to == Wall {
       if let Some(other) = lines.pop() {
         println!("  Considering                {:?}...", other);
-        if (line.start != other.end) || (other.end_connects_to != Wall) {
+        if (line.end != other.start) || (other.start_connects_to != Wall) {
           println!("  Breaking!");
           break;
         }
